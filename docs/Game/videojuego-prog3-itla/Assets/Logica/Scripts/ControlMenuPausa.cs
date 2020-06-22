6bf7794c7f3ed4bf0d1323f80b0b3a821849d8b1
migueldelgadosully <20175587@itla.edu.do>
2020-06-22 00:44:20 -0400

    Creacion de MenuPaused
    Crecion del scritp de menupaused
    y el camvas paused con los botones y importacion de algunos archivos

---
+++ docs/Game/videojuego-prog3-itla/Assets/Logica/Scripts/ControlMenuPausa.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenuPausa : MonoBehaviour
+{
+   public bool pausa = false;
+   public GameObject MenuPausa;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p") && pausa == false)
+        {
+            pausa = true;
+            MenuPausa.active = true;
+            Time.timeScale = 0;
+        }
+    else if (Input.GetKeyDown("p") && pausa == false)
+    
+    {
+        pausa = false;
+            MenuPausa.active = false;
+            Time.timeScale = 1;
+    }
+    }
+}
