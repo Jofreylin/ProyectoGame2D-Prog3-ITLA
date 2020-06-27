501b27ab4feeff1f2d3eac0233606e390b84baeb
Jofreylin <trabajosjofrey@hotmail.com>
2020-06-26 22:40:53 -0400

    updateeee

--- docs/Game/videojuego-prog3-itla/Assets/Logica/Scripts/GeneratorGlobos.cs
+++ docs/Game/videojuego-prog3-itla/Assets/Logica/Scripts/GeneratorGlobos.cs
@@ -6,41 +6,42 @@ public class GeneratorGlobos : MonoBehaviour
 {
 
 
-public GameObject  globo;
+    public GameObject globo;
 
 
-public float generatorTimerspanw = 4.75f;
-public float generatortimerstar = 0.5f;
+    public float generatorTimerspanw = 4.75f;
+    public float generatortimerstar = 0.5f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-       InvokeRepeating  ("CreateGlobos", generatortimerstar, generatorTimerspanw);
-  
+        InvokeRepeating("CreateGlobos", generatortimerstar, generatorTimerspanw);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    void CreateGlobos(){
-
-Instantiate(globo, transform.position, Quaternion.identity);
 
     }
-   
 
+    void CreateGlobos()
+    {
 
-
-
-
-
-
-
-
+        Instantiate(globo, transform.position, Quaternion.identity);
 
     }
+
+
+
+
+
+
+
+
+
+
+
+}
 
